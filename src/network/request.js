8d0380a747006514c10fb295b0299a3e18addefa
unknown <2691495636@qq.com>
2021-08-04 10:31:55 +0800

    ordinary commit

--- src/network/request.js
+++ src/network/request.js
@@ -6,7 +6,7 @@ let ajaxTimer = 0;
 export function request(config) {
   $store.commit('showLoading');
   const install = axios.create({
-    baseURL: 'https://netease-cloud-music-api-nu-red.vercel.app/',
+    baseURL: 'http://1.15.24.13:3000/',
     timeout: 5000,
   });
   install.interceptors.request.use(
